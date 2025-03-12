4045f45786d24f8e7e6ccde9d446abd2c76914b5
Edwin Rubio <edwin.rubioing@outlook.com>
2025-03-12 15:10:33 -0500

    feat: New configuration of cors

--- utils/main.go
+++ utils/main.go
@@ -214,3 +214,33 @@ func GetFileKind(fileType string) string {
 	// Si no es ninguno de los tipos admitidos
 	return ""
 }
+
+// CORSMiddleware configura los encabezados necesarios para permitir peticiones CORS
+func CORSMiddleware(cors_urls string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		allowedOrigins := strings.Split(cors_urls, ",")
+		origin := c.Request.Header.Get("Origin")
+		
+		// Verifica si el origen está en la lista de permitidos
+		for _, allowedOrigin := range allowedOrigins {
+			if strings.TrimSpace(allowedOrigin) == origin {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			break
+			}
+		}
+    
+        
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+	}
+}
+
